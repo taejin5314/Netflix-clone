fff98482192d441d2ad03a526dc0ab42506b9365
taejin <taejin5314@gmail.com>
2020-09-20 15:48:06 -0400

    First commit

---
+++ src/components/row/Row.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import './Row.css';
+import axios from '../../axios';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
+    const [Movies, setMovies] = useState([]);
+    const [TrailerUrl, setTrailerUrl] = useState('');
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
+
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: {
+            autoplay: 1,
+        },
+    }
+
+    const handleClick = (movie) => {
+        if (TrailerUrl) {
+            setTrailerUrl('');
+        } else {
+            movieTrailer(movie?.name || "")
+                .then((url) => {
+                    console.log(url)
+                    const urlParams = new URLSearchParams(new URL(url).search);
+                    setTrailerUrl(urlParams.get('v'));
+                }).catch(error => console.log(error))
+        }
+    }
+
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+
+            <div className="row__posters">
+                {Movies.map(movie => (
+                    <img
+                        key={movie.id}
+                        onClick={() => handleClick(movie)}
+                        className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                        alt={movie.name}
+                    />
+                ))}
+            </div>
+
+            {TrailerUrl && <YouTube videoId={TrailerUrl} opts={opts} />}
+        </div>
+    )
+}
+
+export default Row
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCG8hsiZn-XHKkHUIbZ_TFsCOLC0K9IiDw",
+    authDomain: "netflix-clone-f46d6.firebaseapp.com",
+    databaseURL: "https://netflix-clone-f46d6.firebaseio.com",
+    projectId: "netflix-clone-f46d6",
+    storageBucket: "netflix-clone-f46d6.appspot.com",
+    messagingSenderId: "550533720850",
+    appId: "1:550533720850:web:dd931c9bad3d37a65be1cb",
+    measurementId: "G-G0QGN7BLRZ"
+};
